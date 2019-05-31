6f3f99b63f77c72add2741df96088215e0d67b68
alexey <prog5@gerchikco.com>
2019-05-31 18:19:26 +0300

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchFormComponent } from './components/search-form/search-form.component';
+import { SearchResultsComponent } from './components/search-results/search-results.component';
+import { PaginationComponent } from './components/pagination/pagination.component';
+import { SearchResultItemComponent } from './components/search-result-item/search-result-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchFormComponent,
+    SearchResultsComponent,
+    PaginationComponent,
+    SearchResultItemComponent
   ],
   imports: [
     BrowserModule,
